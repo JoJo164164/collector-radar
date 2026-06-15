ab327e2a31363610e4a97692392ff0d3ce9b15cd
JoJo164164 <jamison0219@yahoo.com.tw>
2026-06-15 10:45:34 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, String, Float, Integer, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+
+engine = create_engine("sqlite:///collector.db", echo=False)
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    price = Column(Float)
+    platform = Column(String)
+    url = Column(String, unique=True)
+    image = Column(String)
+    time = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
