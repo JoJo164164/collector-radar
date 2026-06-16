568971d7feab4aae2869372a174540d77765f6c1
JoJo164164 <jamison0219@yahoo.com.tw>
2026-06-16 16:17:43 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,24 +1,22 @@
-from scraper import (
-    scrape_yahoo,
-    scrape_ebay,
-    scrape_shopee,
-    scrape_mercari
-)
+from scraper_shopee import scrape_shopee
+from scraper_mercari import scrape_mercari
+from scraper_ebay import scrape_ebay
+from scraper_yahoo import scrape_yahoo
 
 def search_all(keyword, sources):
 
     results = []
-
-    if "yahoo" in sources:
-        results += scrape_yahoo(keyword)
-
-    if "ebay" in sources:
-        results += scrape_ebay(keyword)
 
     if "shopee" in sources:
         results += scrape_shopee(keyword)
 
     if "mercari" in sources:
         results += scrape_mercari(keyword)
+
+    if "ebay" in sources:
+        results += scrape_ebay(keyword)
+
+    if "yahoo" in sources:
+        results += scrape_yahoo(keyword)
 
     return results
