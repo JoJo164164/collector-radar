7c4a15e997db0e7f2dbc812919f55a38fb7c7b22
JoJo164164 <jamison0219@yahoo.com.tw>
2026-06-16 10:01:33 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -37,24 +37,30 @@ def clean_price(text):
 # =========================
 # PLAYWRIGHT FETCH
 # =========================
+from playwright.sync_api import sync_playwright
+
 def fetch_html(url):
     with sync_playwright() as p:
         browser = p.chromium.launch(
-    headless=True,
-    args=[
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu"
-    ]
-)
+            headless=True,
+            args=[
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+            ]
+        )
+
         page = browser.new_page()
 
         page.goto(url, timeout=60000)
-        page.wait_for_timeout(2500)
+
+        page.wait_for_timeout(2000)
 
         html = page.content()
+
         browser.close()
+
         return html
 
 
